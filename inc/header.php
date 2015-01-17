2dea0508bd98bcb53764ae7144d30962ef24f5d8
Pranu Sarna <pranusarna@gmail.com>
2015-01-18 02:23:37 +0530

    one style

--- inc/header.php
+++ inc/header.php
@@ -7,11 +7,6 @@
 		<title><?php echo "$pageName" ; ?></title>
 		<link rel="stylesheet" type="text/css" href="styles/normalize.css">
 		<link rel="stylesheet" type="text/css" href="styles/style.css">
-		<link rel="stylesheet" type="text/css" href="styles/responsive.css">
-		<link rel="stylesheet" type="text/css" href="styles/responsiveAboutContact.css">
-		<link rel="stylesheet" type="text/css" href="styles/responsiveProducts.css">
-		<link rel="stylesheet" type="text/css" href="styles/responsiveForm.css">
-		<link rel="stylesheet" type="text/css" href="styles/responsiveFooter.css">
 		<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.3.15/slick.css"/>
 
 
