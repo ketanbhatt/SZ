a78ee405cdca403c2c90ce2b55c1f882ae1ccb4a
Ketan Bhatt <ktbt10@gmail.com>
2015-01-18 03:15:05 +0530

    Revert "Revert "Content  for the site""
    This reverts commit 00923321c19eaf2c2fe5454081f25bf0b89bd4aa.

--- inc/items.php
+++ inc/items.php
@@ -11,7 +11,7 @@
 				can be used for carrying ingredients, raw materials, etc in Food industry. 
 				We can provide Steel Bin with Cover or without cover and in different shapes 
 				and sizes. We can provide Steel Bin in bulk quantities and that too at 
-				industry leading rates. 
+				industry leading rates.<br><br> 
 				Material Used : SS 316, SS 304, SS 202 & Mild Steel"
 			),
 		array(
@@ -20,7 +20,7 @@
 				in New Delhi. Our Steel Laboratory Table is corrosion-resistant and can keep 
 				Machines, equipment and spares. We make sure to test Steel Laboratory Table 
 				before purchasing and also before final delivery to the customers. Clients can 
-				avail Steel Laboratory Table from us at competitive rates. 
+				avail Steel Laboratory Table from us at competitive rates. <br><br>
 				Material Used : SS 316, SS 304, SS 202 & Mild Steel and Aluminum"
 			),
 		array(
@@ -28,7 +28,7 @@
 			"desc" => "We are amidst reputed Wholesale Suppliers of Steel Squat Pan which 
 				is perfectly designed as per user’s specification. The Steel Squat Pan, we 
 				provide, is made with excellent quality material, and hence can sustain for 
-				long. We can take up bulk orders for Steel Squat Pan. 
+				long. We can take up bulk orders for Steel Squat Pan. <br><br>
 				Material Used : SS 304"
 			)
 		);
@@ -39,12 +39,12 @@
 			"desc" => "Sheet Metal Components provided by us, are designed as per international 
 				standards, thus widely demanded throughout the market. Sheet Metal Components, 
 				that we offer, have flawless design owing to which these can withstand tough 
-				circumstances also.
-				Highlights
+				circumstances also.<br><br>
+				Highlights:<br>
 
-				•	Quality-tested
-				•	Sturdy built
-				•	Highly durable
+				•	Quality-tested<br>
+				•	Sturdy built<br>
+				•	Highly durable<br>
 				•	Market leading prices"
 			)
 		);
@@ -56,20 +56,20 @@
 				We offer an extensive array of Kitchen Equipment such as Bain Marie and Canteen Table. Food 
 				containers are available in different capacities such as 15 liters and 20 liters. Kitchen 
 				Equipment that we offer is needed at several places like Hotel industry, restaurants, canteens 
-				etc. to keep food. We can provide Kitchen Equipment at any mentioned location.
-				Material Used : SS 304 or SS 202 Customer specific
+				etc. to keep food. We can provide Kitchen Equipment at any mentioned location.<br><br>
+				Material Used : SS 304 or SS 202 Customer specific<br><br>
 
-				Specialties
+				Specialties:<br>
 
-				•	Flawless design
-				•	User friendly
-				•	Sustain extreme conditions
-				•	Industry leading prices
+				•	Flawless design<br>
+				•	User friendly<br>
+				•	Sustain extreme conditions<br>
+				•	Industry leading prices<br><br>
 
-				Range
+				Range:<br>
 
-				•	Hot water bath
-				•	Gas range
+				•	Hot water bath<br>
+				•	Gas range<br>
 				•	Water Storage"
 			)
 		);
@@ -80,13 +80,13 @@
 			"desc" => "We are well-recognized as noted Custom Built Components Wholesale Supplier in New Delhi. 
 				We offer a gamut of Custom Built Components that comprises of Power Roller, Springs and Machined 
 				Components. Our Custom Built Components are in conformity with high industrial standards, 
-				therefore widely demanded throughout the market. We can offer Custom Built Components at affordable prices.
+				therefore widely demanded throughout the market. We can offer Custom Built Components at affordable prices.<br><br>
 
-				Highlights
+				Highlights:<br>
 
-				•	Excellent service life
-				•	Accurate design
-				•	Withstand extreme conditions
+				•	Excellent service life<br>
+				•	Accurate design<br>
+				•	Withstand extreme conditions<br>
 				•	Competitive rates"
 			)
 		);
@@ -97,21 +97,21 @@
 			"desc" => "We are counted amongst credible Wholesale Suppliers of Material Handling Conveyors in New Delhi. 
 				We offer an extensive range of Material Handling Conveyors that includes Expandable Roller Conveyor, Gravity 
 				Roller Conveyor, Portable Loader Conveyor and Telescopic Belt Conveyor. Our Material Handling Conveyors are 
-				functional at extreme conditions also. Therefore, Material Handling Conveyors that we offer, are widely in demand.
+				functional at extreme conditions also. Therefore, Material Handling Conveyors that we offer, are widely in demand.<br><br>
 
-				Material Used : Stainless Steel and Mild Steel
+				Material Used : Stainless Steel and Mild Steel<br><br>
 
-				Application Areas
+				Application Areas:<br>
 
-				•	Food industry
-				•	Electronic industry
-				•	Automobile industry
+				•	Food industry<br>
+				•	Electronic industry<br>
+				•	Automobile industry<br><br>
 
-				Specialties
+				Specialties:<br>
 
-				•	Longer working life
-				•	Perfectly designed
-				•	Can sustain to tough conditions
+				•	Longer working life<br>
+				•	Perfectly designed<br>
+				•	Can sustain to tough conditions<br>
 				•	Affordable rates"
 			)
 		);
@@ -121,21 +121,21 @@
 			"name" => "",
 			"desc" => "We are offering the best quality Metal Press Components to the clients all over the country. 
 				Metal Press Components, provided by us, are dimensionally accurate and have sturdy construction. 
-				We can deliver Metal Press Components in bulk quantities.
+				We can deliver Metal Press Components in bulk quantities.<br><br>
 
-				Material Used : MS, SS, Brass and Aluminum
+				Material Used : MS, SS, Brass and Aluminum<br><br>
 
-				Application Areas
+				Application Areas:<br>
 
-				•	Electronic industry
-				•	Food industry
-				•	Automobile industry
+				•	Electronic industry<br>
+				•	Food industry<br>
+				•	Automobile industry<br><br>
 
-				Highlights
+				Highlights:<br>
 
-				•	Precision engineered
-				•	Reliable working life
-				•	Withstand tough conditions
+				•	Precision engineered<br>
+				•	Reliable working life<br>
+				•	Withstand tough conditions<br>
 				•	Reasonable rates"
 			)
 		);
@@ -146,21 +146,21 @@
 			"desc" => "We are reckoned as reliable Wholesale Supplier of Plastic Moulded Components in New Delhi. Our 
 				Plastic Moulded Components are in compliance with high quality standard of the market. Plastic Moulded 
 				Components that we offer find their applications in diverse industrial units. We can provide Plastic 
-				Moulded Components at industry leading rates.
+				Moulded Components at industry leading rates.<br><br>
 
-				Material Used : Nylon, PVC and any kind of molding material
+				Material Used : Nylon, PVC and any kind of molding material<br><br>
 
-				Application Areas
+				Application Areas:<br>
 
-				•	Food industry
-				•	Electronic industry
-				•	Automobile industry
+				•	Food industry<br>
+				•	Electronic industry<br>
+				•	Automobile industry<br><br>
 
-				Specialties
+				Specialties:<br>
 
-				•	Robust construction
-				•	Flawless design structure
-				•	Quality tested
+				•	Robust construction<br>
+				•	Flawless design structure<br>
+				•	Quality tested<br>
 				•	Market leading rates"
 			)
 		);
@@ -169,21 +169,21 @@
 		array(
 			"name" => "",
 			"desc" => "We are engaged in supplying excellent quality Rubber O Rings to the nationwide customers. We can 
-				take up bulk orders of Rubber O Rings and ensure to provide it at any specified location. 
+				take up bulk orders of Rubber O Rings and ensure to provide it at any specified location. <br><br>
 
-				Material Used : Rubber, Neoprene and Silicone
+				Material Used : Rubber, Neoprene and Silicone<br><br>
 
-				Application Areas
+				Application Areas:<br>
 
-				•	Automobile industry
-				•	Food industry
-				•	Electronic industry
+				•	Automobile industry<br>
+				•	Food industry<br>
+				•	Electronic industry<br><br>
 
-				Highlights
+				Highlights:<br>
 
-				•	Quality checked
-				•	Sturdy built
-				•	Durable
+				•	Quality checked<br>
+				•	Sturdy built<br>
+				•	Durable<br>
 				•	Competitive rates"
 			)
 		);
